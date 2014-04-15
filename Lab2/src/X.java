1e7fd340720b3a4ab26f3eee26b6d04e8df50179
Nathan Flint <nlflint@gmail.com>
2014-04-15 14:07:15 -0700

    Lab2: Added layouts and fixed drawing routine.

--- Lab2/src/X.java
+++ Lab2/src/X.java
@@ -16,19 +16,19 @@ public class X extends javax.swing.JComponent {
      * @param g The Graphics object use to render
      */
     public void paintComponent(java.awt.Graphics g) {
-        // paint the background
         super.paintComponent(g);
-        // draw the two lines
-        g.drawLine(0, 0, getWidth(), getHeight());
-        g.drawLine(0, getHeight(), getWidth(), 0);
+        g.drawLine(0, 0, getWidth() - 1, getHeight() - 1);
+        g.drawLine(0, getHeight() - 1, getWidth() - 1, 0);
     }
 
     public static void main(String[] args)
     {
         javax.swing.JFrame win;
         win = new javax.swing.JFrame("Test Component");
-        X x = new X();
-        win.add(x);
+        win.add(new X(), java.awt.BorderLayout.NORTH);
+        win.add(new X(), java.awt.BorderLayout.SOUTH);
+        win.add(new X(), java.awt.BorderLayout.EAST);
+        win.add(new X(), java.awt.BorderLayout.WEST);
         win.setSize(400, 300);
         win.setVisible(true);
     }
