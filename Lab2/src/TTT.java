f2273d12b5712ba83c43a507b50d14a2f46b875c
Nathan Flint <nlflint@gmail.com>
2014-04-17 21:00:52 -0700

    Lab2: Created new implementation for lines

--- Lab2/src/TTT.java
+++ Lab2/src/TTT.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
 
 /**
@@ -8,16 +9,20 @@ import java.awt.*;
  * @version Lab2: Graphics
  */
 public class TTT {
-    private JPanel ticTacToePanel;
+    private TicTacToePanel ticTacToePanel;
     private JComponent parentComponent;
 
     public TTT()
     {
         // Create a 3x3 layout
         GridLayout grid = new GridLayout(3,3);
+        grid.setHgap(20);
+        grid.setVgap(20);
 
         // Create the tic tac toe board and apply the grid layout
-        ticTacToePanel = new JPanel(grid);
+        ticTacToePanel = new TicTacToePanel();
+        ticTacToePanel.setLayout(grid);
+        ticTacToePanel.setBorder(new EmptyBorder(10,10,10,10));
 
         // Create a window
         JFrame win = new JFrame("Tic-Tac-Toe");
@@ -41,100 +46,71 @@ public class TTT {
     {
         // Add X's and O's to the tic tac toe board. The Draw enum also determines which lines
         // of the tic tac toe board will be drawn.
-        ticTacToePanel.add(CreateXCell(Draw.rightAndBottomLines));
-        ticTacToePanel.add(CreateOCell(Draw.rightAndBottomLines));
-        ticTacToePanel.add(CreateXCell(Draw.bottomLineOnly));
-        ticTacToePanel.add(CreateOCell(Draw.rightAndBottomLines));
-        ticTacToePanel.add(CreateXCell(Draw.rightAndBottomLines));
-        ticTacToePanel.add(CreateTicTacToeCell(null, Draw.bottomLineOnly));
-        ticTacToePanel.add(CreateOCell(Draw.rightLineOnly));
-        ticTacToePanel.add(CreateTicTacToeCell(null, Draw.rightLineOnly));
-        ticTacToePanel.add(CreateXCell(Draw.noLines));
+        ticTacToePanel.add(CreateXCell());
+        ticTacToePanel.add(CreateOCell());
+        ticTacToePanel.add(CreateXCell());
+        ticTacToePanel.add(CreateOCell());
+        ticTacToePanel.add(CreateXCell());
+        ticTacToePanel.add(CreateTicTacToeCell(null));
+        ticTacToePanel.add(CreateOCell());
+        ticTacToePanel.add(CreateTicTacToeCell(null));
+        ticTacToePanel.add(CreateXCell());
 
         ticTacToePanel.validate();
     }
 
-    // Creates an "X" with the given tic tac toe lines
-    private JComponent CreateXCell(Draw lines)
+    // Creates an "X"
+    private JComponent CreateXCell()
     {
         X2 xComponent = new X2();
         xComponent.setLineColor(Color.blue);
 
-        return CreateTicTacToeCell(xComponent, lines);
+        return CreateTicTacToeCell(xComponent);
     }
 
-    private TicTacToeCell CreateTicTacToeCell(Component shape, Draw lines)
+    // Creates a JPanel with some default spacing and attaches an optional component
+    private JPanel CreateTicTacToeCell(Component shape)
     {
         // Create the cell, which is a JPanel, and give it some spacing around it.
-        TicTacToeCell cell = new TicTacToeCell(lines);
+        JPanel cell = new JPanel();
         cell.setLayout(new BorderLayout());
         cell.setBorder(BorderFactory.createEmptyBorder(13,13,13,13));
+        cell.setOpaque(false);
 
-        // If a shape was given, then add it. Else it's an un-played tile
+        // If a shape was given, then add it. Else it's a blank tile
         if (shape != null)
             cell.add(shape);
 
         return cell;
     }
 
-    // Creates an "O" with the given tic tac toe lines
-    private JComponent CreateOCell(Draw lines)
+    // Creates an "O"
+    private JComponent CreateOCell()
     {
         O2 oComponent = new O2();
         oComponent.setLineColor(Color.red);
         oComponent.setFillColor(Color.yellow);
 
-        return CreateTicTacToeCell(oComponent, lines);
+        return CreateTicTacToeCell(oComponent);
     }
 
     /**
-     * Each tic tac toe cell draws components contained inside which are set elsewhere,
-     * and then optionally draws a right and bottom line for tic tac toe lines.
+     * Creates a panel with tic tac toe lines drawn in the background
      */
-    private class TicTacToeCell extends JPanel {
-        // Contains the setting for which tic tac toe lines to draw
-        private Draw draw;
-
-        /**
-         * Constructor saves the line draw setting.
-         * @param lines Which lines will be drawn
-         */
-        public TicTacToeCell(Draw lines) {
-            super();
-
-            // Save the line draw settings
-            draw = lines;
-        }
-
+    private class TicTacToePanel extends JPanel {
         /**
          * Draws the tic tac toe lines.
          * @param g graphics for drawing
          */
         public void paintComponent(Graphics g)
         {
-            // Optionally draw the right line
-            if (draw == Draw.rightAndBottomLines || draw == Draw.rightLineOnly)
-                g.drawLine(getWidth() - 1, 0, getWidth() - 1, getHeight() - 1);
+            // Draws vertical tic tac toe lines
+            g.drawLine(getWidth() / 3, 0, getWidth() / 3, getHeight());
+            g.drawLine(getWidth() * 2 / 3, 0, getWidth() * 2 / 3, getHeight());
 
-            // Optionally draw the bottom line
-            if (draw == Draw.rightAndBottomLines || draw == Draw.bottomLineOnly)
-                g.drawLine(0, getHeight() - 1, getWidth() - 1, getHeight() - 1);
+            // Draws horizontal tic tac toe lines
+            g.drawLine(0, getHeight() / 3, getWidth(), getHeight() / 3);
+            g.drawLine(0, getHeight() * 2 / 3, getWidth(), getHeight() * 2 / 3);
         }
     }
-
-    /**
-     * Each tic tac toe cell will optionally draw a bottom line and right lin
-     * for the tic tac toe lines.
-     *
-     * This enum identifies the possibilities.
-     */
-    private enum Draw
-    {
-        rightLineOnly,
-        bottomLineOnly,
-        rightAndBottomLines,
-        noLines
-
-    }
-
 }
