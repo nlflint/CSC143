fb76df658df0b2698251f472a7e1e9d016974db7
Nathan Flint <nlflint@gmail.com>
2014-04-18 18:09:29 -0700

    Assignment 2: Fixed bug where Pixel could not have colors > 255 or < 0.

--- Assignment2/src/Gaussian.java
+++ Assignment2/src/Gaussian.java
@@ -10,6 +10,8 @@
  */
 public class Gaussian implements Filter
 {
+    // Used to index an array of int for color
+    private final static int RED = 0, GREEN = 1, BLUE = 2;
 
     // Describes the 3x3 transform in 1 array. this method will be customized by inheriting filters.
     protected int[] getTransform()
@@ -64,39 +66,39 @@ public class Gaussian implements Filter
     private Pixel transformPixels(Pixel[] pixels)
     {
         // Transform the pixels into a new set of colors
-        Pixel newPixel = transformPixelsToColors(pixels);
+        int[] newColors = transformPixelsToColors(pixels);
 
         // Adjust weights by transform total
-        adjustColorWeights(newPixel);
+        adjustColorWeights(newColors);
 
         // Adjust all colors to be between 0 and 255
-        constrainColors(newPixel);
+        constrainColors(newColors);
 
-        return newPixel;
+        return new Pixel(newColors[RED], newColors[GREEN], newColors[BLUE]);
     }
 
     // Uses the transform to create a new set of colors
-    private Pixel transformPixelsToColors(Pixel[] pixels)
+    private int[] transformPixelsToColors(Pixel[] pixels)
     {
         // Get custom transform.
         int[] transform = getTransform();
 
         // where the new colors will be stored
-        int red = 0, green = 0, blue = 0;
+        int[] colors = new int[] {0, 0, 0};
 
         // Transform each pixel to the new colors.
         for (int i = 0; i < pixels.length; i++)
         {
-            red += pixels[i].rgb[Pixel.RED] * transform[i];
-            green += pixels[i].rgb[Pixel.GREEN] * transform[i];
-            blue += pixels[i].rgb[Pixel.BLUE] * transform[i];
+            colors[RED] += pixels[i].rgb[Pixel.RED] * transform[i];
+            colors[GREEN] += pixels[i].rgb[Pixel.GREEN] * transform[i];
+            colors[BLUE] += pixels[i].rgb[Pixel.BLUE] * transform[i];
         }
 
-        return new Pixel(red, green, blue);
+        return colors;
     }
 
     // Adjusts the colors by the total weight of the transform
-    private void adjustColorWeights(Pixel newPixel)
+    private void adjustColorWeights(int[] colors)
     {
         int[] transform = getTransform();
 
@@ -106,18 +108,18 @@ public class Gaussian implements Filter
             transformTotalWeight += transform[i];
         }
 
-        newPixel.rgb[Pixel.RED] /= transformTotalWeight;
-        newPixel.rgb[Pixel.GREEN] /= transformTotalWeight;
-        newPixel.rgb[Pixel.BLUE] /= transformTotalWeight;
+        colors[RED] /= transformTotalWeight;
+        colors[GREEN] /= transformTotalWeight;
+        colors[BLUE] /= transformTotalWeight;
     }
 
     // Insures colors are 0-255
-    private void constrainColors(Pixel newPixel)
+    private void constrainColors(int[] colors)
     {
-        for (int i = 0; i < newPixel.rgb.length; i++)
+        for (int i = 0; i < colors.length; i++)
         {
-            newPixel.rgb[i] = Math.max(newPixel.rgb[i], 0);
-            newPixel.rgb[i] = Math.min(newPixel.rgb[i], 255);
+            colors[i] = Math.max(colors[i], 0);
+            colors[i] = Math.min(colors[i], 255);
         }
 
     }
