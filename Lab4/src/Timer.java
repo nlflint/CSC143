bbc148b0be9f0c01234ba63ddd87a73d7fe15ab2
Nathan Flint <nlflint@gmail.com>
2014-04-29 16:48:58 -0700

    Lab4: Implemented Lab4 initial solution.

--- Lab4/src/Timer.java
+++ Lab4/src/Timer.java
@@ -1,14 +1,17 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  * Created by nathanf on 4/28/14.
  */
-public class Timer
+public class Timer implements ActionListener
 {
     JButton startPause;
     JButton reset;
-    JLabel time;
+    JLabel timeText;
+    TimeController timer;
 
     public Timer ()
     {
@@ -19,24 +22,116 @@ public class Timer
         window.setTitle("My Timer");
 
         startPause = new JButton("Start");
-        reset = new JButton("Reset");
+        startPause.addActionListener(this);
 
-        time = new JLabel("0.00 seconds");
-        time.setFont(new Font("Ariel", Font.BOLD, 36));
-        time.setHorizontalAlignment(JLabel.CENTER);
+        reset = new JButton("Reset");
+        reset.addActionListener(this);
+
+        timeText = new JLabel();
+        timeText.setFont(new Font("Ariel", Font.BOLD, 36));
+        timeText.setHorizontalAlignment(JLabel.CENTER);
 
         JPanel topArea = new JPanel();
         topArea.add(startPause);
         topArea.add(reset);
 
         window.add(topArea, BorderLayout.NORTH);
-        window.add(time, BorderLayout.CENTER);
+        window.add(timeText, BorderLayout.CENTER);
 
         window.setVisible(true);
+
+        timer = new TimeController(timeText);
     }
 
     public static void main(String[] args)
     {
         new Timer();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e)
+    {
+        JButton source = (JButton) e.getSource();
+
+        if (source == startPause && source.getText().equals("Start"))
+        {
+            timer.start();
+            startPause.setText("Pause");
+        }
+        else if (source == startPause && source.getText().equals("Pause"))
+        {
+            timer.pause();
+            startPause.setText("Start");
+        }
+        else if (source == reset)
+        {
+            timer.reset();
+            startPause.setText("Start");
+        }
+    }
+
+    private class TimeController implements Runnable
+    {
+        double currentTime;
+        boolean isTimerRunning;
+        Thread timerThread;
+        JLabel timeLabel;
+
+        public TimeController(JLabel timeLabel)
+        {
+            this.timeLabel = timeLabel;
+            reset();
+        }
+        public void start()
+        {
+            timerThread = new Thread(this);
+            timerThread.start();
+            isTimerRunning = true;
+        }
+
+        public void pause()
+        {
+            isTimerRunning = false;
+        }
+
+        public void reset()
+        {
+            isTimerRunning = false;
+            currentTime = 0.0;
+            refreshTimeLabel();
+        }
+
+        private void refreshTimeLabel()
+        {
+            timeLabel.setText(String.format("%.2f seconds", currentTime));
+        }
+
+        @Override
+        public void run() {
+            while (isTimerRunning)
+            {
+                incrementTimeLabel();
+                sleep();
+            }
+        }
+
+        private void incrementTimeLabel() {
+            currentTime += 0.01;
+            refreshTimeLabel();
+        }
+
+        private void sleep() {
+            try
+            {
+                Thread.sleep(10);
+            }
+            catch (InterruptedException e)
+            {
+                //error happened, stop timer
+                isTimerRunning = false;
+            }
+        }
+
+
     }
 }
