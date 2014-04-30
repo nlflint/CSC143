1dc27fa71e46951e0562dc4903f6d741395bbb22
Nathan Flint <nlflint@gmail.com>
2014-04-30 13:28:29 -0700

    Lab4: Finished lab

--- Lab4/src/Timer.java
+++ Lab4/src/Timer.java
@@ -1,137 +1,283 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.*;
+import java.util.ArrayList;
 
 /**
- * Created by nathanf on 4/28/14.
+ * Stop watch UI
+ *
+ * @author Nathan Flint
+ * @version Lab4: Timer
  */
 public class Timer implements ActionListener
 {
-    JButton startPause;
-    JButton reset;
-    JLabel timeText;
-    TimeController timer;
+    // Start / Pause / Continue button
+    private JButton startPause;
 
+    // Reset button
+    private JButton reset;
+
+    // Text that shows time
+    private JLabel timeText;
+
+    // The underlying stop watch
+    private StopWatch timer;
+
+    // Static text of buttons
+    private static final String START = "Start";
+    private static final String PAUSE = "Pause";
+    private static final String CONTINUE = "Continue";
+    private static final String RESET = "Reset";
+
+    /**
+     * Constructor. Sets up UI and stop watch
+     */
     public Timer ()
     {
+        configureUI();
+        configureStopWatch();
+    }
+
+    // Sets up up window and buttons
+    private void configureUI()
+    {
+        // make window
         JFrame window = new JFrame();
         window.setSize(400, 300);
         window.setLocation(100, 100);
         window.setLayout(new BorderLayout());
         window.setTitle("My Timer");
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        startPause = new JButton("Start");
+        // Build buttons
+        startPause = new JButton(START);
         startPause.addActionListener(this);
-
-        reset = new JButton("Reset");
+        reset = new JButton(RESET);
         reset.addActionListener(this);
 
+        // Build label for showing elapsed time
         timeText = new JLabel();
         timeText.setFont(new Font("Ariel", Font.BOLD, 36));
         timeText.setHorizontalAlignment(JLabel.CENTER);
+        timeText.setText("0.00 seconds");
 
+        // This panel holds buttons along top of window area
         JPanel topArea = new JPanel();
         topArea.add(startPause);
         topArea.add(reset);
 
+        // Add items to window
         window.add(topArea, BorderLayout.NORTH);
         window.add(timeText, BorderLayout.CENTER);
-
         window.setVisible(true);
-
-        timer = new TimeController(timeText);
     }
 
+    // Sets up the the stop watch
+    private void configureStopWatch()
+    {
+        long millisecondsPerTic = 10;
+        timer = new StopWatch(millisecondsPerTic);
+        // Stopwatch will trigger events on every tic.
+        timer.addTickEvent(new StopWatchTickEvent() {
+            @Override
+            public void tick() {
+                timeText.setText(String.format("%.2f seconds", timer.getElapsedTime()));
+            }
+        });
+    }
+
+    /**
+     * Application method
+     * @param args no command line args are not implemented.
+     */
     public static void main(String[] args)
     {
+        // Starts program
         new Timer();
     }
 
+    /**
+     * Handles logic for when any buttons are clicked
+     * @param e information about the event
+     */
     @Override
     public void actionPerformed(ActionEvent e)
     {
+        // Gathering basic info from the event
         JButton source = (JButton) e.getSource();
+        String buttonText = ((JButton) e.getSource()).getText();
 
-        if (source == startPause && source.getText().equals("Start"))
+        // start or Continue button were pressed.
+        if (source == startPause
+                && (buttonText.equals(START) || buttonText.equals(CONTINUE)))
         {
+            // start/continue the stop watch. behaviors are identical.
             timer.start();
-            startPause.setText("Pause");
+            // Change button to say pause
+            startPause.setText(PAUSE);
         }
-        else if (source == startPause && source.getText().equals("Pause"))
+        // Pause was pressed
+        else if (source == startPause && source.getText().equals(PAUSE))
         {
+            // Pause stop watch
             timer.pause();
-            startPause.setText("Start");
+            // Change button to say continue
+            startPause.setText(CONTINUE);
         }
+        // Reset was pressed
         else if (source == reset)
         {
+            // reset stopwatch
             timer.reset();
-            startPause.setText("Start");
+            // Change button to say Start
+            startPause.setText(START);
+            // Set focus to start button
+            startPause.grabFocus();
         }
     }
+}
 
-    private class TimeController implements Runnable
+/**
+ * Implements a stop watch
+ *
+ * @author Nathan Flint
+ * @version Lab4: Timer
+ */
+class StopWatch
+{
+    // time reading from the stop watch
+    private double elapsedSeconds;
+
+    // Flag tracks if stop watch is running
+    private boolean timerIsRunning;
+
+    // Tread the stop watch uses while running
+    private Thread timerThread;
+
+    // Collection of events that are triggered
+    private ArrayList<StopWatchTickEvent> tickEvents;
+
+    // Time between stop watch tics
+    private long ticMilliseconds;
+
+    /**
+     * Constructor. Sets up initial settings, and resets stop watch.
+     * @param milliseconds
+     */
+    public StopWatch(long milliseconds)
     {
-        double currentTime;
-        boolean isTimerRunning;
-        Thread timerThread;
-        JLabel timeLabel;
+        tickEvents = new ArrayList<StopWatchTickEvent>();
+        ticMilliseconds = milliseconds;
+        timerIsRunning = false;
+        elapsedSeconds = 0.0;
+    }
 
-        public TimeController(JLabel timeLabel)
+    /**
+     * Stops watch and sets elapsed time to 0.
+     */
+    public void reset()
+    {
+        timerIsRunning = false;
+        elapsedSeconds = 0.0;
+        triggerTickEvents();
+    }
+
+    // Tiggers every event in the collection
+    private void triggerTickEvents() {
+        for (StopWatchTickEvent event : tickEvents)
+            event.tick();
+    }
+
+    /**
+     * Starts the stop watch.
+     * Continues if watch is paused.
+     * Does nothing if watch is already running.
+     */
+    public void start()
+    {
+        if (timerIsRunning)
+            return;
+
+        timerThread = new Thread(
+                new Runnable() {
+                    public void run() {
+                        while (timerIsRunning)
+                        {
+                            incrementTime();
+                            sleep();
+                        }
+                    }
+                });
+
+        timerThread.start();
+        timerIsRunning = true;
+    }
+
+    // Increments elapsed time by tic amount
+    private void incrementTime() {
+        elapsedSeconds += ticMilliseconds / 1000.0;
+        triggerTickEvents();
+    }
+
+    // Sleeps for the tic amount
+    private void sleep() {
+        // how long we want to sleep for.
+        long millisecondsRemaining = ticMilliseconds;
+        // When the wait time is over
+        long wakeUpTime = System.currentTimeMillis() + ticMilliseconds;
+
+        // Loop until we are past the wake up time. In case thread is awaken earlier than instructed.
+        while (millisecondsRemaining > 0)
         {
-            this.timeLabel = timeLabel;
-            reset();
-        }
-        public void start()
-        {
-            timerThread = new Thread(this);
-            timerThread.start();
-            isTimerRunning = true;
-        }
-
-        public void pause()
-        {
-            isTimerRunning = false;
-        }
-
-        public void reset()
-        {
-            isTimerRunning = false;
-            currentTime = 0.0;
-            refreshTimeLabel();
-        }
-
-        private void refreshTimeLabel()
-        {
-            timeLabel.setText(String.format("%.2f seconds", currentTime));
-        }
-
-        @Override
-        public void run() {
-            while (isTimerRunning)
-            {
-                incrementTimeLabel();
-                sleep();
-            }
-        }
-
-        private void incrementTimeLabel() {
-            currentTime += 0.01;
-            refreshTimeLabel();
-        }
-
-        private void sleep() {
             try
             {
-                Thread.sleep(10);
+                // Sleep for the time remaining
+                Thread.sleep(millisecondsRemaining);
             }
             catch (InterruptedException e)
             {
                 //error happened, stop timer
-                isTimerRunning = false;
+                timerIsRunning = false;
             }
+            // update time left until wakeup time.
+            millisecondsRemaining = wakeUpTime - System.currentTimeMillis();
         }
-
-
     }
+
+    /**
+     * Pauses stop watch if it's running.
+     * Does nothing if it's already paused.
+     */
+    public void pause()
+    {
+        timerIsRunning = false;
+    }
+
+    /**
+     * Gets time elapsed in seconds
+     * @return elapsed time in seconds
+     */
+    public double getElapsedTime()
+    {
+        return elapsedSeconds;
+    }
+
+    /**
+     * Adds an event to the event list.
+     * @param event Event to be triggered at every tic
+     */
+    public void addTickEvent(StopWatchTickEvent event)
+    {
+        tickEvents.add(event);
+    }
+}
+
+/**
+ * Interface for event triggered on every tic of the stop watch
+ */
+interface StopWatchTickEvent {
+    /**
+     * Method is executed every time event is fired
+     */
+    public void tick();
 }
