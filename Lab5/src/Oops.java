921422a8c7d1212666bc7cb3bab2115bddc7b8a8
Nathan Flint <nlflint@gmail.com>
2014-05-05 18:00:55 -0700

    Lab5: Added initial project

---
+++ Lab5/src/Oops.java
@@ -0,0 +1,36 @@
+public class Oops {
+  
+//    Instatatiate a RuntimeException here.
+  RuntimeException x;
+  
+  public Oops() {
+//    Instatatiate a RuntimeException here.
+    x = new RuntimeException("Instantiated in the constructor");
+  }
+  
+  public void m1() {
+    m2();
+  }
+  
+  public void m2() {
+//    Instatatiate a RuntimeException here.
+    x = new RuntimeException("Instantiated in m2");
+    m3();
+  }
+  
+  public void m3() {
+    m4();
+  }
+  
+  public void m4() {
+//    Instatatiate a RuntimeException here.
+    x = new RuntimeException("Instantiated in m4");
+    throw x;
+  }
+  
+  public static void main(String[] args) {
+//    Instatatiate a RuntimeException here.
+    new Oops().m1();
+  }
+  
+}
