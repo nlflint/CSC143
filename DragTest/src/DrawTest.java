35d1abe600dae9f13b42a06ec4a43921e3366c49
Nathan Flint <nlflint@gmail.com>
2014-05-07 08:23:47 -0700

    Drag test: attempted to use layeredPanel

--- DragTest/src/DrawTest.java
+++ DragTest/src/DrawTest.java
@@ -4,6 +4,7 @@ import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -14,6 +15,7 @@ import java.net.URL;
  */
 public class DrawTest {
     private JFrame window;
+    private JLayeredPane pane;
     private DraggablePanel card;
 
     public DrawTest() {
@@ -22,9 +24,12 @@ public class DrawTest {
         window.setLayout(null);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+        pane = new JLayeredPane();
+        pane.setPreferredSize(new Dimension(500,500));
+
         BufferedImage image = null;
         try {
-            InputStream cardImageFile = new FileInputStream("C:\\git\\csc143\\DragTest\\out\\production\\DragTest\\cardback.jpg");
+            InputStream cardImageFile = new FileInputStream("cardback.jpg");
             image = ImageIO.read(cardImageFile);
         }
         catch (Exception e)
@@ -32,15 +37,12 @@ public class DrawTest {
             System.out.println(e.getMessage());
         }
 
+        for (int i = 0; i < 5; i++)
+        {
+            pane.add(new DraggablePanel(image, pane), JLayeredPane.DEFAULT_LAYER);
+        }
 
-
-        card = new DraggablePanel(image);
-        window.add(card);
-        card.setLocation(100,100);
-
-
-
-
+        window.add(pane);
         window.setVisible(true);
     }
 
@@ -54,17 +56,14 @@ class DraggablePanel extends JPanel {
     private int offsetX;
     private int offsetY;
     private Image image;
+    private JLayeredPane pane;
 
-    public void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        g.drawImage(image,0,0, null);
-    }
-
-    public DraggablePanel(Image image) {
+    public DraggablePanel(Image image, JLayeredPane pane) {
         super();
         setSize(image.getWidth(null), image.getHeight(null));
         this.image = image;
-        setBackground(Color.red);
+        this.pane = pane;
+
         addMouseMotionListener(new MouseAdapter() {
             @Override
             public void mouseDragged(MouseEvent e) {
@@ -83,7 +82,22 @@ class DraggablePanel extends JPanel {
                 super.mousePressed(e);
                 offsetX = e.getX();
                 offsetY = e.getY();
+                moveToLayer(JLayeredPane.DRAG_LAYER);
+
+            }
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                moveToLayer(JLayeredPane.DEFAULT_LAYER);
             }
         });
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawImage(image,0,0, null);
+    }
+
+    private void moveToLayer(int JLayerPaneIndex) {
+        pane.setLayer(this, JLayerPaneIndex);
     }
 }
