1deb1e801a3435fc7da709a7a7eede110026391c
Nathan Flint <nlflint@gmail.com>
2014-05-10 11:01:22 -0700

    Assignment5: Added initial project

---
+++ Assignment5/src/NumericSupport.java
@@ -0,0 +1,19 @@
+/**
+ * Support for numeric output for SudokuView, optional.
+ */
+public interface NumericSupport {
+    
+    /**
+     * Sets the output type: True for numeric output, False
+     * for symbol output.
+     * @param flag The desired output type
+     */
+    public void setNumeric(boolean flag);
+    
+    /**
+     * Retrieve the current output type, numeric or graphic
+     * @return True if numeric output, False if symbols are output
+     */
+    public boolean showsNumeric();
+    
+}
---
+++ Assignment5/src/SelectedCell.java
@@ -0,0 +1,26 @@
+/**
+ * Interface to support the programmatic control of the
+ * SelectedCell in SudokuView.
+ */
+public interface SelectedCell {
+
+    /**
+     * Set the selected cell to the given row and column.
+     * @param row The indicated row
+     * @param col The indicated column
+     */
+    public void setSelected(int row, int col);
+
+    /**
+     * Retrive the row of the currently selected cell.
+     * @return The row in which the selected cell is located.
+     */
+    public int getSelectedRow();
+
+    /**
+     * Retrive the column of the currently selected cell.
+     * @return The column in which the selected cell is located.
+     */
+    public int getSelectedColumn();
+
+}
